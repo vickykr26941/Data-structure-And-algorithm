c1675c05dc85006ef07ab0354747fbe3cffd7586
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2022-06-02 16:05:17 +0530

    greedy

---
+++ greedy/Number of Flowers in Full Bloom.cpp
@@ -0,0 +1,41 @@
+// date : 02/06/2022
+
+
+
+
+class Solution {
+public:
+    
+   
+    // time : nlogn
+    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& persons) {
+        sort(begin(flowers),end(flowers));
+        
+        vector<vector<int>> rev_flowers = flowers;
+        sort(begin(rev_flowers),end(rev_flowers),[&](vector<int> &a , vector<int> &b){ return a[1] < b[1]; });
+        // sort(begin(persons),end(persons));
+        
+        using pii = pair<int,int>;
+        multiset<pii> st;
+        vector<pii> ordered_persons;
+        for(int i = 0; i < (int)persons.size(); i++){
+            ordered_persons.push_back({persons[i],i});
+        }
+        sort(begin(ordered_persons),end(ordered_persons));
+        
+        int i = 0, j = 0, n = persons.size();
+        vector<int> res(persons.size(), 0);
+        for(int k = 0; k < n; k++){
+            while(i < flowers.size() && flowers[i][0] <= ordered_persons[k].first){
+                st.insert({flowers[i][0], flowers[i][1]});
+                i ++;
+            }
+            while(j < rev_flowers.size() && rev_flowers[j][1] < ordered_persons[k].first){
+                st.erase({rev_flowers[j][0], rev_flowers[j][1]});
+                j++;
+            }
+            res[ordered_persons[k].second] = st.size();
+        }
+        return res;
+    }
+};
